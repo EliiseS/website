feebe84606a253ce2423aee75555409739238769
Eliise S <selingeliise@gmail.com>
2020-07-21 21:14:16 +0000

    Use proper https

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32m Deploying updates to GitHub...\033[0m\n"
 
 cd website/public
 filesChanged=$(git status --porcelain=v1 2>/dev/null | wc -l)
@@ -16,11 +16,11 @@ git config user.email "actions@github.com"
 git config user.name "Github Actions"
 
 # Add changes to git.
-printf "\033[0;Add changes to git...\033[0m\n"
+printf "\033[0;32m Add changes to git...\033[0m\n"
 git add .
 
 # Commit changes.
-printf "\033[0;32mCommiting changes...\033[0m\n"
+printf "\033[0;32m Commiting changes...\033[0m\n"
 msg="Updating site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
@@ -28,11 +28,11 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-printf "\033[0;Push site submodule...\033[0m\n"
-git push origin master
+printf "\033[0;32m Push site submodule...\033[0m\n"
+git push "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/EliiseS/eliises.github.io.git"
 
-printf "\033[0;Add submodule changes to this repo...\033[0m\n"
+printf "\033[0;32m Add submodule changes to this repo...\033[0m\n"
 cd -
 git add .
 git commit -m "$msg"
-git push
+git push "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
